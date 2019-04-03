06d6a7eff8bce6d233c63e47ed9ead6d36a5a3ca
Jose Gomez <jose4gga@gmail.com>
2019-04-02 23:32:57 -0400

    Endpoints for generate and redirect for shorten urls

--- app/controllers/shortener_controller.rb
+++ app/controllers/shortener_controller.rb
@@ -1,2 +1,28 @@
 class ShortenerController < ApplicationController
+
+  def create
+    params.require(:url)
+    url = params[:url].chomp "/"
+
+    website = Website.find_or_initialize_by(url: url)
+
+
+    if website.persisted?
+      render json: { short_url: request.base_url + "/#{website.short_id}", visited_count: website.visited_count }
+    elsif website.save
+      render json: { short_url: request.base_url + "/#{website.short_id}", visited_count: website.visited_count }
+    else
+      render json: { errors: website.errors.values.flatten }
+    end
+  end
+
+  def show
+    website = Website.find_by_short_id(params[:id])
+    website.update(visited_count: website.visited_count + 1)
+    redirect_to website.url =~ /https?:\/\/[\S]+/ ? website.url : "//#{website.url}"
+  end
+
+  rescue_from ActionController::ParameterMissing do |exception|
+    render json: { "errors" => ["#{exception.param} is required"] }, status: 400
+  end
 end
--- db/migrate/20190402185558_create_websites.rb
+++ db/migrate/20190402185558_create_websites.rb
@@ -1,6 +1,7 @@
 class CreateWebsites < ActiveRecord::Migration[5.2]
   def change
     create_table :websites do |t|
+      t.text :title
       t.string :url, null: false
       t.boolean :processing_title, default: true
       t.integer :visited_count, default: 0
